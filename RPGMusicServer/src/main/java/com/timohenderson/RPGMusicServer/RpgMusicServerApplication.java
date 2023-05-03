e72ec59982aaec1b7d0d5cb0b837da48092bd3d8
TimoHenderson <tjhenderson2@gmail.com>
2023-05-03 09:09:54 +0100

    Initial Commit

---
+++ RPGMusicServer/src/main/java/com/timohenderson/RPGMusicServer/RpgMusicServerApplication.java
@@ -0,0 +1,13 @@
+package com.timohenderson.RPGMusicServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgMusicServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgMusicServerApplication.class, args);
+	}
+
+}
---
+++ RPGMusicServer/src/test/java/com/timohenderson/RPGMusicServer/RpgMusicServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timohenderson.RPGMusicServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgMusicServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
