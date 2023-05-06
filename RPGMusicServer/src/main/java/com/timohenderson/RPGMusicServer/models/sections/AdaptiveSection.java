580bf2de9a5a951215540d00129376b1a9dea80d
TimoHenderson <tjhenderson2@gmail.com>
2023-05-06 12:45:12 +0100

    encapsulate section

--- RPGMusicServer/src/main/java/com/timohenderson/RPGMusicServer/models/sections/AdaptiveSection.java
+++ RPGMusicServer/src/main/java/com/timohenderson/RPGMusicServer/models/sections/AdaptiveSection.java
@@ -3,26 +3,26 @@ package com.timohenderson.RPGMusicServer.models.sections;
 import com.timohenderson.RPGMusicServer.enums.MusicalType;
 import com.timohenderson.RPGMusicServer.models.parts.Part;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AdaptiveSection extends Section {
-    private HashMap<MusicalType, List<Part>> partLists;
+    HashMap<MusicalType, List<Part>> partListsMap;
 
 
-    public AdaptiveSection(String name, SectionData sectionData, HashMap<MusicalType, List<Part>> partLists) {
+    public AdaptiveSection(String name, SectionData sectionData, HashMap<MusicalType, List<Part>> partListsMap) {
         super(name, sectionData);
-
-        this.partLists = partLists;
+        this.partListsMap = partListsMap;
     }
-
 
     @Override
     public List<Part> getNextParts(HashMap<String, Integer> partsNeeded) {
         return null;
     }
 
-    public HashMap<MusicalType, List<Part>> getPartLists() {
-        return partLists;
+    public Map<MusicalType, List<Part>> getPartLists() {
+        return Collections.unmodifiableMap(partListsMap);
     }
 }
